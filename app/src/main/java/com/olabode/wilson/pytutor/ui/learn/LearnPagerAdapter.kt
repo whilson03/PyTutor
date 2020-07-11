1224e6e469feaca334919ee0eadc3082449dfbc3
Adeyoriju Olabode Wilson <whilson03@gmail.com>
2020-07-11 12:19:42 +0100

    implemented viewpager2 for navigating between basic topics and advanced topics
    Signed-off-by: Adeyoriju Olabode Wilson <whilson03@gmail.com>

---
+++ app/src/main/java/com/olabode/wilson/pytutor/ui/learn/LearnPagerAdapter.kt
@@ -0,0 +1,26 @@
+package com.olabode.wilson.pytutor.ui.learn
+
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+/**
+ *   Created by OLABODE WILSON on 7/11/20.
+ */
+
+private const val NUM_PAGES = 2
+
+class LearnPagerAdapter(fa: Fragment) : FragmentStateAdapter(fa) {
+    override fun getItemCount(): Int {
+        return NUM_PAGES
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        return when (position) {
+            0 -> BasicFragment()
+            else -> {
+                AdvancedFragment()
+            }
+        }
+    }
+
+}
