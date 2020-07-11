57ee53cf4bfa1836acac4ef72a380ddaa2706f2e
Adeyoriju Olabode Wilson <whilson03@gmail.com>
2020-07-11 12:19:42 +0100

    implemented viewpager2 for navigating between basic topics and advanced topics
    Signed-off-by: Adeyoriju Olabode Wilson <whilson03@gmail.com>

--- app/src/main/java/com/olabode/wilson/pytutor/ui/learn/LearnFragment.kt
+++ app/src/main/java/com/olabode/wilson/pytutor/ui/learn/LearnFragment.kt
@@ -5,14 +5,38 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 import com.olabode.wilson.pytutor.R
+import com.olabode.wilson.pytutor.databinding.FragmentLearnBinding
 
 class LearnFragment : Fragment() {
 
+    private var _binding: FragmentLearnBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var tabLayout: TabLayout
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_learn, container, false)
+        _binding = FragmentLearnBinding.inflate(inflater, container, false)
+
+        tabLayout = binding.tabLayout
+        binding.pager.adapter = LearnPagerAdapter(this)
+        TabLayoutMediator(tabLayout, binding.pager) { tab, position ->
+            when (position) {
+                0 -> tab.text = getString(R.string.basic)
+                else -> tab.text = getString(R.string.advanced)
+            }
+        }.attach()
+
+
+        return binding.root
+    }
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 }
