abe072df9c1485142fa94b12175a2d1c576caf7a
Adeyoriju Olabode Wilson <whilson03@gmail.com>
2020-09-06 00:07:36 +0100

    setup firebase
    Signed-off-by: Adeyoriju Olabode Wilson <whilson03@gmail.com>

--- app/src/main/java/com/olabode/wilson/pytutor/di/AppModule.kt
+++ app/src/main/java/com/olabode/wilson/pytutor/di/AppModule.kt
@@ -1,8 +1,13 @@
 package com.olabode.wilson.pytutor.di
 
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ApplicationComponent
+import javax.inject.Singleton
 
 /**
  *   Created by OLABODE WILSON on 8/11/20.
@@ -10,4 +15,15 @@ import dagger.hilt.android.components.ApplicationComponent
 
 @Module
 @InstallIn(ApplicationComponent::class)
-object AppModule
+object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideFirebaseAuth() = FirebaseAuth.getInstance()
+
+    @Singleton
+    @Provides
+    fun provideFirebaseFireStore() = Firebase.firestore
+
+
+}
