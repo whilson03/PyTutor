3f916d290186a097fce058961ad0e798f6c7669d
Adeyoriju Olabode Wilson <whilson03@gmail.com>
2020-09-05 23:52:57 +0100

    implemented sign up ui
    Signed-off-by: Adeyoriju Olabode Wilson <whilson03@gmail.com>

---
+++ app/src/main/java/com/olabode/wilson/pytutor/ui/auth/signup/SignUpFragment.kt
@@ -0,0 +1,19 @@
+package com.olabode.wilson.pytutor.ui.auth.signup
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.olabode.wilson.pytutor.R
+
+
+class SignUpFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_sign_up, container, false)
+    }
+
+}
