75dc36b4998af1d236f16524bfaaea29390b147e
Adeyoriju Olabode Wilson <whilson03@gmail.com>
2020-09-05 23:29:42 +0100

    implemented login ui
    Signed-off-by: Adeyoriju Olabode Wilson <whilson03@gmail.com>

---
+++ app/src/main/java/com/olabode/wilson/pytutor/ui/auth/AuthActivity.kt
@@ -0,0 +1,12 @@
+package com.olabode.wilson.pytutor.ui.auth
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.olabode.wilson.pytutor.R
+
+class AuthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+    }
+}
