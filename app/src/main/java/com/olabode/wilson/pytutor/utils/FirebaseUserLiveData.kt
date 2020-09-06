d49c46adb4bb54d6125f205420eee34d6d2b09e8
Adeyoriju Olabode Wilson <whilson03@gmail.com>
2020-09-06 01:29:39 +0100

    modified user class
    Signed-off-by: Adeyoriju Olabode Wilson <whilson03@gmail.com>

---
+++ app/src/main/java/com/olabode/wilson/pytutor/utils/FirebaseUserLiveData.kt
@@ -0,0 +1,26 @@
+package com.olabode.wilson.pytutor.utils
+
+import androidx.lifecycle.LiveData
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+
+class FirebaseUserLiveData(private val firebaseAuth: FirebaseAuth) : LiveData<FirebaseUser?>() {
+
+
+    private val authStateListener = FirebaseAuth.AuthStateListener { firebaseAuth ->
+        value = firebaseAuth.currentUser
+    }
+
+
+    // When this object has an active observer, start observing the FirebaseAuth state to see if
+    // there is currently a logged in user.
+    override fun onActive() {
+        firebaseAuth.addAuthStateListener(authStateListener)
+    }
+
+    // When this object no longer has an active observer, stop observing the FirebaseAuth state to
+    // prevent memory leaks.
+    override fun onInactive() {
+        firebaseAuth.removeAuthStateListener(authStateListener)
+    }
+}
---
+++ app/src/main/java/com/olabode/wilson/pytutor/utils/RemoteDatabaseKeys.kt
@@ -0,0 +1,9 @@
+package com.olabode.wilson.pytutor.utils
+
+/**
+ *   Created by OLABODE WILSON on 9/6/20.
+ */
+object RemoteDatabaseKeys {
+
+    const val NODE_USERS = "USERS"
+}
