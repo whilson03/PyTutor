066698c4971623c16a71c4f453af9d9d96e68a44
rhodeon <ruonabraks@gmail.com>
2020-09-08 00:24:49 +0100

    Added view and placeholder resources for achievements

--- app/src/main/java/com/olabode/wilson/pytutor/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/olabode/wilson/pytutor/ui/profile/ProfileFragment.kt
@@ -5,20 +5,40 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.olabode.wilson.pytutor.R
 import com.olabode.wilson.pytutor.databinding.FragmentProfileBinding
+import com.olabode.wilson.pytutor.models.AchievementItem
 
 class ProfileFragment : Fragment() {
     private var _binding: FragmentProfileBinding? = null
     private val binding get() = _binding!!
+    private lateinit var achievementsListAdapter: AchievementsListAdapter
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         _binding = FragmentProfileBinding.inflate(inflater, container, false)
+        achievementsListAdapter = AchievementsListAdapter()
+        binding.achievementRecycler.adapter = achievementsListAdapter
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        achievementsListAdapter.submitList(getDummyList())
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    fun getDummyList(): List<AchievementItem> {
+        return List(8) {AchievementItem(R.drawable.ic_baseline_stars_24, "Baby Steps", "Completed your first lesson")}
+//        return listOf(
+//                AchievementItem(R.drawable.ic_baseline_stars_24, "Baby Steps", "Completed your first lesson"),
+//                AchievementItem(R.drawable.ic_baseline_stars_24, "Baby Steps", "Completed your first lesson"),
+//                AchievementItem(R.drawable.ic_baseline_stars_24, "Baby Steps", "Completed your first lesson"),
+//                AchievementItem(R.drawable.ic_baseline_stars_24, "Baby Steps", "Completed your first lesson")
+//        )
     }
 
 
