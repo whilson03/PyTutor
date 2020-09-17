d397715f3975b51f6842d6c9d2b47ca44ced13af
Adeyoriju Olabode Wilson <whilson03@gmail.com>
2020-09-17 17:46:45 +0100

    implemented quiz completion screen (code needs cleanup)
    Signed-off-by: Adeyoriju Olabode Wilson <whilson03@gmail.com>

---
+++ app/src/main/java/com/olabode/wilson/pytutor/ui/tutorial/LessonCompletionFragment.kt
@@ -0,0 +1,56 @@
+package com.olabode.wilson.pytutor.ui.tutorial
+
+import android.animation.ObjectAnimator
+import android.graphics.Color
+import android.os.Bundle
+import android.util.DisplayMetrics
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import com.olabode.wilson.pytutor.R
+import com.olabode.wilson.pytutor.databinding.FragmentLessonCompletionBinding
+import com.olabode.wilson.pytutor.extensions.viewBinding
+import nl.dionsegijn.konfetti.models.Shape
+import nl.dionsegijn.konfetti.models.Size
+
+
+class LessonCompletionFragment : Fragment(R.layout.fragment_lesson_completion) {
+    private val binding by viewBinding(FragmentLessonCompletionBinding::bind)
+
+    private val args: LessonCompletionFragmentArgs by navArgs()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val display = DisplayMetrics()
+        requireActivity().windowManager.defaultDisplay.getMetrics(display)
+        val score = args.score
+        val numberOfQuestions = args.numberOfQuestions
+
+        binding.viewKonfetti.build()
+                .addColors(Color.YELLOW, Color.GREEN, Color.MAGENTA)
+                .setDirection(0.0, 359.0)
+                .setSpeed(1f, 5f)
+                .setFadeOutEnabled(true)
+                .setTimeToLive(2000L)
+                .addShapes(Shape.Square, Shape.Circle)
+                .addSizes(Size(12))
+                .setPosition(-50f, display.widthPixels + 50f, -50f, -50f)
+                .streamFor(300, 5000L)
+
+
+        binding.home.setOnClickListener {
+            findNavController().popBackStack()
+        }
+
+        setUpRating(score.toFloat())
+    }
+
+
+    private fun setUpRating(value: Float) {
+        val current: Float = binding.ratingBar.rating
+        val anim = ObjectAnimator.ofFloat(binding.ratingBar, "rating", current, value)
+        anim.duration = 2000
+        anim.start()
+    }
+}
--- app/src/main/java/com/olabode/wilson/pytutor/ui/tutorial/QuestionFragment.kt
+++ app/src/main/java/com/olabode/wilson/pytutor/ui/tutorial/QuestionFragment.kt
@@ -1,58 +1,192 @@
 package com.olabode.wilson.pytutor.ui.tutorial
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import android.widget.RadioButton
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.olabode.wilson.pytutor.R
+import com.olabode.wilson.pytutor.databinding.FragmentQuestionBinding
+import com.olabode.wilson.pytutor.extensions.viewBinding
+import com.olabode.wilson.pytutor.models.tutorial.LessonResponse
+import com.olabode.wilson.pytutor.models.tutorial.Question
+import dagger.hilt.android.AndroidEntryPoint
+import java.util.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+@AndroidEntryPoint
+class QuestionFragment : Fragment(R.layout.fragment_question) {
+    private val binding by viewBinding(FragmentQuestionBinding::bind)
+    private lateinit var lessonQuestionResponse: LessonResponse
+    private var currentQuestionIndex = 0
+    private var isQuizAnswered = false
+    private var score = 0
+    private var answeredKeys = ArrayList<String>()
 
-/**
- * A simple [Fragment] subclass.
- * Use the [QuestionFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class QuestionFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    companion object {
+        const val QUESTION_RESPONSE = "QuestionResponse"
+        const val KEY_SAVE_QUESTIONS = "QUESTIONS"
+        const val KEY_CURRENT_INDEX = "KEY_CURRENT_INDEX"
+        const val KEY_COMPLETED_QUIZ = "KEY_COMPLETED_QUIZ"
+        const val KEY_SCORE = "SCORE"
+        const val LIST_OF_KEYS_ANSWERED = "LIST_OF_KEYS_ANSWERED"
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+        @JvmStatic
+        fun newInstance(lessonResponse: LessonResponse) =
+                QuestionFragment().apply {
+                    arguments = Bundle().apply {
+                        putParcelable(QUESTION_RESPONSE, lessonResponse)
+                    }
+                }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        if (savedInstanceState == null) {
+            arguments?.let {
+                lessonQuestionResponse = it.getParcelable(QUESTION_RESPONSE)!!
+            }
+        } else {
+            lessonQuestionResponse = savedInstanceState.getParcelable(KEY_SAVE_QUESTIONS)!!
+            currentQuestionIndex = savedInstanceState.getInt(KEY_CURRENT_INDEX)
+            score = savedInstanceState.getInt(KEY_SCORE)
+            isQuizAnswered = savedInstanceState.getBoolean(KEY_COMPLETED_QUIZ)
+            answeredKeys = savedInstanceState.getStringArrayList(LIST_OF_KEYS_ANSWERED)!!
+        }
+
+        val questions = lessonQuestionResponse.question!!.values.toList()
+
+        setUpQuestion(questions[currentQuestionIndex])
+
+        if (answeredKeys.contains(questions[currentQuestionIndex].question)) {
+            isQuizAnswered = true
+
+            getTextFromCheckedId(binding.optionsGroup.checkedRadioButtonId)
+            val question = questions[currentQuestionIndex]
+            val optionsMap = question.options
+            showRightAndWrongAnswers(optionsMap[question.answerKey] ?: error(""))
+            binding.submitOrNext.text = getString(R.string.next)
+            disableOptions()
+        }
+
+        Toast.makeText(requireContext(), "$score", Toast.LENGTH_SHORT).show()
+
+        binding.submitOrNext.setOnClickListener {
+            if (getString(R.string.finish) != binding.submitOrNext.text.toString()) {
+                if (getString(R.string.next) == binding.submitOrNext.text.toString()) {
+                    moveToNextQuestion(questions)
+                    setUpQuestion(questions[currentQuestionIndex])
+                    resetOptions()
+                    enableOptions()
+                    binding.submitOrNext.text = getString(R.string.submit)
+                } else {
+                    if (binding.option1.isChecked || binding.option2.isChecked
+                            || binding.option3.isChecked) {
+
+                        val selectedAnswer =
+                                getTextFromCheckedId(binding.optionsGroup.checkedRadioButtonId)
+                        validateAnswer(selectedAnswer,
+                                questions[currentQuestionIndex]
+                        )
+                        answeredKeys.add(questions[currentQuestionIndex].question)
+
+                        if (currentQuestionIndex == questions.size.minus(1)) {
+                            navigateToCompletionScreen(score, noOfQuestion = questions.size)
+                        } else {
+                            binding.submitOrNext.text = getString(R.string.next)
+                        }
+                        disableOptions()
+                    } else {
+                        Toast.makeText(
+                                requireContext(),
+                                getString(R.string.pick_option_prompt),
+                                Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+            } else {
+                navigateToCompletionScreen(score, questions.size)
+            }
         }
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_question, container, false)
+    private fun navigateToCompletionScreen(score: Int, noOfQuestion: Int) {
+        findNavController().navigate(ViewTutorialsFragmentDirections
+                .actionViewTutorialsFragmentToLessonCompletionFragment(
+                        score = score, numberOfQuestions = noOfQuestion
+                ))
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment QuestionFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-                QuestionFragment().apply {
-                    arguments = Bundle().apply {
-                        putString(ARG_PARAM1, param1)
-                        putString(ARG_PARAM2, param2)
-                    }
-                }
+
+    private fun setUpQuestion(question: Question) {
+        val options = question.options.values.toList()
+        binding.questionText.text = question.question
+        binding.option1.text = options[0]
+        binding.option2.text = options[1]
+        binding.option3.text = options[2]
+    }
+
+    private fun moveToNextQuestion(questions: List<Question>) {
+        if (currentQuestionIndex < questions.size.minus(1)) {
+            currentQuestionIndex++
+        }
+    }
+
+    private fun validateAnswer(answer: String, question: Question) {
+        val optionsMap = question.options
+        showRightAndWrongAnswers(optionsMap[question.answerKey] ?: error(""))
+
+        if (optionsMap[question.answerKey] == answer) {
+            score++
+        }
+    }
+
+    private fun getTextFromCheckedId(id: Int): String {
+        return when (id) {
+            binding.option1.id -> binding.option1.text.toString()
+            binding.option2.id -> binding.option2.text.toString()
+            else -> binding.option3.text.toString()
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt(KEY_CURRENT_INDEX, currentQuestionIndex)
+        outState.putParcelable(KEY_SAVE_QUESTIONS, lessonQuestionResponse)
+        outState.putBoolean(KEY_COMPLETED_QUIZ, isQuizAnswered)
+        outState.putInt(KEY_SCORE, score)
+        outState.putStringArrayList(LIST_OF_KEYS_ANSWERED, answeredKeys)
+    }
+
+    private fun enableOptions() {
+        binding.optionsGroup.clearCheck()
+        binding.option1.isEnabled = true
+        binding.option2.isEnabled = true
+        binding.option3.isEnabled = true
+    }
+
+    private fun disableOptions() {
+        binding.option1.isEnabled = false
+        binding.option2.isEnabled = false
+        binding.option3.isEnabled = false
+    }
+
+    private fun showRightAndWrongAnswers(answer: String) {
+        for (i in 0 until binding.optionsGroup.childCount) {
+            val rb = binding.optionsGroup.getChildAt(i) as RadioButton
+            if (rb.text.toString() == answer) {
+                rb.setBackgroundResource(R.drawable.option_correct_background)
+            } else {
+                rb.setBackgroundResource(R.drawable.option_wrong_background)
+            }
+        }
+    }
+
+    private fun resetOptions() {
+        for (i in 0 until binding.optionsGroup.childCount) {
+            val rb = binding.optionsGroup.getChildAt(i) as RadioButton
+            rb.setBackgroundResource(R.drawable.quiz_options_selector)
+        }
     }
 }
