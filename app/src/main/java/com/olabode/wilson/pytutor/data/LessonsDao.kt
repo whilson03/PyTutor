018baa816d2dfbee365debf6b9a185a34d7d85b6
Adeyoriju Olabode Wilson <whilson03@gmail.com>
2020-09-18 04:29:42 +0100

    caching implementation in progress
    Signed-off-by: Adeyoriju Olabode Wilson <whilson03@gmail.com>

---
+++ app/src/main/java/com/olabode/wilson/pytutor/data/LessonsDao.kt
@@ -0,0 +1,10 @@
+package com.olabode.wilson.pytutor.data
+
+import androidx.room.Dao
+
+/**
+ *   Created by OLABODE WILSON on 9/17/20.
+ */
+
+@Dao
+interface LessonsDao
---
+++ app/src/main/java/com/olabode/wilson/pytutor/data/PytutorDatabase.kt
@@ -0,0 +1,8 @@
+package com.olabode.wilson.pytutor.data
+
+import androidx.room.RoomDatabase
+
+/**
+ *   Created by OLABODE WILSON on 9/17/20.
+ */
+abstract class PytutorDatabase : RoomDatabase()
