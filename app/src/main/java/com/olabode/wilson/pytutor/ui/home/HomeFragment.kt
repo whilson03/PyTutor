6333de3181516f67dfeb62f16d533a19caec2dec
Adeyoriju Olabode Wilson <whilson03@gmail.com>
2020-09-19 16:54:32 +0100

    home ui revamp
    Signed-off-by: Adeyoriju Olabode Wilson <whilson03@gmail.com>

--- app/src/main/java/com/olabode/wilson/pytutor/ui/home/HomeFragment.kt
+++ app/src/main/java/com/olabode/wilson/pytutor/ui/home/HomeFragment.kt
@@ -9,7 +9,6 @@ import androidx.navigation.fragment.findNavController
 import com.olabode.wilson.pytutor.R
 import com.olabode.wilson.pytutor.databinding.FragmentHomeBinding
 import com.olabode.wilson.pytutor.extensions.viewBinding
-import com.olabode.wilson.pytutor.models.HomeListItem
 import com.olabode.wilson.pytutor.utils.AuthResult
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -19,26 +18,20 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
     private val binding by viewBinding(FragmentHomeBinding::bind)
     private val viewModel: HomeViewModel by viewModels()
 
-    private lateinit var adapter: HomeListAdapter
-
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        adapter = HomeListAdapter {
-            findNavController().navigate(HomeFragmentDirections.actionHomeFragmentToTutorialTopicsFragment())
-        }
-        binding.homeRecycler.adapter = adapter
         observeAuthState()
-        adapter.submitList(dummyList())
-    }
 
-    private fun dummyList(): List<HomeListItem>? =
-            listOf(
-                    HomeListItem(1, R.drawable.ic_profile, "Tutorial"),
-                    HomeListItem(1, R.drawable.ic_profile, "Examples"),
-                    HomeListItem(1, R.drawable.ic_profile, "Questions"),
-                    HomeListItem(1, R.drawable.ic_profile, "Code")
-            )
+        binding.cardOne.setOnClickListener {
+            findNavController().navigate(HomeFragmentDirections
+                    .actionHomeFragmentToTutorialTopicsFragment())
+        }
+
+        binding.cardTwo.setOnClickListener {
+
+        }
+    }
 
 
     private fun observeAuthState() {
