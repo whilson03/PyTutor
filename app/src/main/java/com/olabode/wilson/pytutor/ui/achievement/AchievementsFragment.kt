acb6a92b40a2499d43ea58d49014016edd4ad31a
rhodeon <ruonabraks@gmail.com>
2020-09-25 23:04:07 +0100

    Achievements page navigation fix.

--- app/src/main/java/com/olabode/wilson/pytutor/ui/achievement/AchievementsFragment.kt
+++ app/src/main/java/com/olabode/wilson/pytutor/ui/achievement/AchievementsFragment.kt
@@ -3,6 +3,7 @@ package com.olabode.wilson.pytutor.ui.achievement
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.olabode.wilson.pytutor.R
 import com.olabode.wilson.pytutor.databinding.FragmentAchievementsBinding
 import com.olabode.wilson.pytutor.extensions.viewBinding
@@ -23,6 +24,8 @@ class AchievementsFragment : Fragment(R.layout.fragment_achievements) {
         val achievementsListAdapter = AchievementsListAdapter()
         binding.achievementRecycler.adapter = achievementsListAdapter
         achievementsListAdapter.submitList(getDummyList())
+
+        binding.toolbar.setNavigationOnClickListener { findNavController().navigateUp() }
     }
 
     private fun getDummyList(): List<AchievementItem> = List(16) { AchievementItem(R.drawable.ic_round_stars_24, "Baby Steps", "Completed your first lesson") }
