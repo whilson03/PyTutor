e79f22a7a8eb2820b85a606e5662e73b8b998c25
Adeyoriju Olabode Wilson <whilson03@gmail.com>
2020-09-25 14:02:52 +0100

    profile ui revamp
    Signed-off-by: Adeyoriju Olabode Wilson <whilson03@gmail.com>

--- app/src/main/java/com/olabode/wilson/pytutor/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/olabode/wilson/pytutor/ui/profile/ProfileFragment.kt
@@ -1,12 +1,18 @@
 package com.olabode.wilson.pytutor.ui.profile
 
-import android.view.Menu
-import android.view.MenuInflater
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Observer
+import coil.api.load
+import coil.transform.CircleCropTransformation
 import com.olabode.wilson.pytutor.R
 import com.olabode.wilson.pytutor.databinding.FragmentProfileBinding
 import com.olabode.wilson.pytutor.extensions.viewBinding
+import com.olabode.wilson.pytutor.models.user.User
+import com.olabode.wilson.pytutor.utils.AuthResult
+import com.olabode.wilson.pytutor.utils.DataState
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -15,9 +21,67 @@ class ProfileFragment : Fragment(R.layout.fragment_profile) {
     private val binding by viewBinding(FragmentProfileBinding::bind)
     private val viewModel: ProfileViewModel by activityViewModels()
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        inflater.inflate(R.menu.settings_menu, menu)
+        viewModel.userDetails.observe(viewLifecycleOwner, Observer { result ->
+            when (result) {
+                is DataState.Success -> {
+                    setUpProfile(result.data)
+                }
+            }
+        })
+
+
+        binding.signOut.setOnClickListener {
+            //  logOut()
+        }
+
+        binding.navigateToRate.setOnClickListener {
+
+        }
+
+        binding.navigatePrivacy.setOnClickListener {
+
+        }
+
+        binding.achievement.setOnClickListener {
+
+        }
+
+        binding.editProfile.setOnClickListener {
+
+        }
+
     }
 
+    private fun setUpProfile(user: User) {
+        binding.username.text = user.fullName
+        binding.profileIcon.load(user.imageUrl) {
+            crossfade(true)
+            placeholder(R.drawable.ic_profile_placeholder)
+            transformations(CircleCropTransformation())
+            error(R.drawable.ic_profile_placeholder)
+        }
+    }
+
+
+    private fun logOut() {
+        viewModel.signOut().observe(viewLifecycleOwner, Observer { result ->
+            when (result) {
+                is AuthResult.Success -> {
+
+                }
+
+                is AuthResult.Failed -> {
+
+                }
+
+                is AuthResult.Loading -> {
+
+                }
+            }
+
+        })
+    }
 }
--- app/src/main/java/com/olabode/wilson/pytutor/ui/profile/ProfileViewModel.kt
+++ app/src/main/java/com/olabode/wilson/pytutor/ui/profile/ProfileViewModel.kt
@@ -1,18 +1,27 @@
 package com.olabode.wilson.pytutor.ui.profile
 
 import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
+import com.olabode.wilson.pytutor.repository.auth.AuthRepository
 import com.olabode.wilson.pytutor.repository.main.user.UserRepository
+import com.olabode.wilson.pytutor.utils.AuthResult
 
 /**
  * Created by Ogheneruona Onobrakpeya on 9/23/20.
  */
 
 class ProfileViewModel @ViewModelInject constructor(
-        private val userRepository: UserRepository
+        private val userRepository: UserRepository,
+        private val authRepository: AuthRepository
 ) : ViewModel() {
     val userDetails = userRepository
             .getLoggedInUserDetails(userRepository.getUserId())
             .asLiveData()
+
+
+    fun signOut(): LiveData<AuthResult<String>> {
+        return authRepository.logOut().asLiveData()
+    }
 }
