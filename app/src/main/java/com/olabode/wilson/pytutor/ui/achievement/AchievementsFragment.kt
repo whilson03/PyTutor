a937bad9277d244e4b75920c33d3a9848ed83427
rhodeon <ruonabraks@gmail.com>
2020-09-25 22:05:07 +0100

    Implemented the achievements fragment.

---
+++ app/src/main/java/com/olabode/wilson/pytutor/ui/achievement/AchievementsFragment.kt
@@ -0,0 +1,29 @@
+package com.olabode.wilson.pytutor.ui.achievement
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.olabode.wilson.pytutor.R
+import com.olabode.wilson.pytutor.databinding.FragmentAchievementsBinding
+import com.olabode.wilson.pytutor.extensions.viewBinding
+import com.olabode.wilson.pytutor.models.AchievementItem
+import dagger.hilt.android.AndroidEntryPoint
+
+/**
+ * Created by Ogheneruona Onobrakpeya on 9/25/20.
+ */
+
+@AndroidEntryPoint
+class AchievementsFragment : Fragment(R.layout.fragment_achievements) {
+    private val binding by viewBinding(FragmentAchievementsBinding::bind)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val achievementsListAdapter = AchievementsListAdapter()
+        binding.achievementRecycler.adapter = achievementsListAdapter
+        achievementsListAdapter.submitList(getDummyList())
+    }
+
+    private fun getDummyList(): List<AchievementItem> = List(16) { AchievementItem(R.drawable.ic_round_stars_24, "Baby Steps", "Completed your first lesson") }
+}
