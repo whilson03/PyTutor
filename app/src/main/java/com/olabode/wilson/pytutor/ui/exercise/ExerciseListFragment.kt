88189d1a480cb884ce26bd0df4cef71caf52087c
rhodeon <ruonabraks@gmail.com>
2020-10-13 00:17:31 +0100

    Set the initial bindings for the exercises list fragment with placeholder data.

--- app/src/main/java/com/olabode/wilson/pytutor/ui/exercise/ExerciseListFragment.kt
+++ app/src/main/java/com/olabode/wilson/pytutor/ui/exercise/ExerciseListFragment.kt
@@ -1,9 +1,14 @@
 package com.olabode.wilson.pytutor.ui.exercise
 
+import android.os.Bundle
+import android.view.View
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.olabode.wilson.pytutor.R
 import com.olabode.wilson.pytutor.databinding.FragmentExerciseListBinding
 import com.olabode.wilson.pytutor.extensions.viewBinding
+import com.olabode.wilson.pytutor.models.Exercise
 
 /**
  * Created by Ogheneruona Onobrakpeya on 10/10/20.
@@ -11,4 +16,46 @@ import com.olabode.wilson.pytutor.extensions.viewBinding
 class ExerciseListFragment : Fragment(R.layout.fragment_exercise_list) {
     private val binding by viewBinding(FragmentExerciseListBinding::bind)
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.progressBar.isVisible = false
+
+        val exerciseListAdapter = ExerciseListAdapter {exercise ->
+            findNavController().
+            navigate(ExerciseListFragmentDirections.actionExerciseListFragmentToExerciseFragment(exercise))
+        }
+
+        binding.exerciseListRecycler.adapter = exerciseListAdapter
+        binding.exerciseListRecycler.isVisible = true
+        exerciseListAdapter.submitList(getDummyList())
+        binding.toolbar.setNavigationOnClickListener {findNavController().navigateUp()}
+    }
+
+    fun getDummyList() : List<Exercise> {
+        return listOf(
+                Exercise("1", "Palindrome check", "Easy", "Write a program which checks if a word is a palindrome.", "def palindrome(word):\n" +
+                        "    left = 0\n" +
+                        "    right = len(word) - 1\n" +
+                        "\n" +
+                        "    while left < right:\n" +
+                        "        if word[left] != word[right]:\n" +
+                        "            return False\n" +
+                        "        left += 1\n" +
+                        "        right -= 1\n" +
+                        "\n" +
+                        "    return True\n" +
+                        "\n" +
+                        "def main():\n" +
+                        "    word = input(\"Enter a word\\n\")\n" +
+                        "\n" +
+                        "    if palindrome(word):\n" +
+                        "        print(f\"{word} is a palindrome\")\n" +
+                        "    else:\n" +
+                        "        print(f\"{word} is not a palindrome\")\n" +
+                        "\n" +
+                        "main()"),
+                Exercise("1", "Subtracting", "Medium", "Subtract These", "Add Add"),
+                Exercise("1", "Dividing", "Hard", "Divide These", "Add Add")
+        )
+    }
 }
