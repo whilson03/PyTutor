dad7e3d60015a0a68e0f4425be6b3aba70714b7e
rhodeon <ruonabraks@gmail.com>
2020-10-12 23:51:39 +0100

    Set the bindings for the views in the exercise fragment.

--- app/src/main/java/com/olabode/wilson/pytutor/ui/exercise/ExerciseFragment.kt
+++ app/src/main/java/com/olabode/wilson/pytutor/ui/exercise/ExerciseFragment.kt
@@ -2,7 +2,9 @@ package com.olabode.wilson.pytutor.ui.exercise
 
 import android.os.Bundle
 import android.view.View
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
 import com.olabode.wilson.pytutor.R
 import com.olabode.wilson.pytutor.databinding.FragmentExerciseContentBinding
 import com.olabode.wilson.pytutor.extensions.viewBinding
@@ -12,8 +14,22 @@ import com.olabode.wilson.pytutor.extensions.viewBinding
  */
 class ExerciseFragment: Fragment(R.layout.fragment_exercise_content) {
     private val binding by viewBinding(FragmentExerciseContentBinding::bind)
+    private val args: ExerciseFragmentArgs by navArgs()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val exercise = args.exercise
+        binding.toolbar.title = exercise.title
+        binding.questionText.text = exercise.question
+        binding.solutionView.setCode(
+            formattedSourceCode = exercise.solution,
+            language = "py",
+            showLineNumbers = true
+        )
+
+        binding.showSolutionButton.setOnClickListener {
+            binding.solutionView.isVisible = !binding.solutionView.isVisible
+        }
     }
 }
