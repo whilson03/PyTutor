95195d749901e79f23575fbc59d9b8769fb01e3b
rhodeon <ruonabraks@gmail.com>
2020-10-12 23:50:34 +0100

    Set the initial bindings for the exercises list fragment with placeholder data.

--- app/src/main/java/com/olabode/wilson/pytutor/ui/exercise/ExerciseListFragment.kt
+++ app/src/main/java/com/olabode/wilson/pytutor/ui/exercise/ExerciseListFragment.kt
@@ -1,9 +1,14 @@
 package com.olabode.wilson.pytutor.ui.exercise
 
+import android.os.Bundle
+import android.view.View
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.olabode.wilson.pytutor.R
 import com.olabode.wilson.pytutor.databinding.FragmentExerciseListBinding
 import com.olabode.wilson.pytutor.extensions.viewBinding
+import com.olabode.wilson.pytutor.models.Exercise
 
 /**
  * Created by Ogheneruona Onobrakpeya on 10/10/20.
@@ -11,4 +16,26 @@ import com.olabode.wilson.pytutor.extensions.viewBinding
 class ExerciseListFragment : Fragment(R.layout.fragment_exercise_list) {
     private val binding by viewBinding(FragmentExerciseListBinding::bind)
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.progressBar.isVisible = false
+
+        val exerciseListAdapter = ExerciseListAdapter {exercise ->
+            findNavController().
+            navigate(ExerciseListFragmentDirections.actionExerciseListFragmentToExerciseFragment(exercise))
+        }
+
+        binding.exerciseListRecycler.adapter = exerciseListAdapter
+        binding.exerciseListRecycler.isVisible = true
+        exerciseListAdapter.submitList(getDummyList())
+        binding.toolbar.setNavigationOnClickListener {findNavController().navigateUp()}
+    }
+
+    fun getDummyList() : List<Exercise> {
+        return listOf(
+                Exercise("1", "Adding", "Easy", "Add These", "Add Add"),
+                Exercise("1", "Subtracting", "Medium", "Subtract These", "Add Add"),
+                Exercise("1", "Dividing", "Hard", "Divide These", "Add Add")
+        )
+    }
 }
