9f63e028e2ab2a9abf44654f4668aba351b93b7e
rhodeon <ruonabraks@gmail.com>
2020-10-12 22:12:40 +0100

    Created the exercise list related files and updated the model to include difficulty level.

---
+++ app/src/main/java/com/olabode/wilson/pytutor/ui/exercise/ExerciseListAdapter.kt
@@ -0,0 +1,48 @@
+package com.olabode.wilson.pytutor.ui.exercise
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.olabode.wilson.pytutor.databinding.ItemExercisesBinding
+import com.olabode.wilson.pytutor.models.Exercise
+import com.olabode.wilson.pytutor.utils.Utils
+
+/**
+ * Created by Ogheneruona Onobrakpeya on 10/10/20.
+ */
+
+class ExerciseListAdapter(private val clickListener: (exercise: Exercise) -> Unit) :
+    ListAdapter<Exercise, ExerciseListAdapter.ViewHolder>(Exercise.DIFF_CALLBACK) {
+
+    class ViewHolder(
+        private val binding: ItemExercisesBinding,
+        private val clickListener: (exercise: Exercise) -> Unit
+    ) : RecyclerView.ViewHolder(binding.root) {
+        private var item: Exercise? = null
+
+        init {
+            binding.root.setOnClickListener {
+                item?.let {
+                     clickListener.invoke(it)
+                }
+            }
+        }
+
+        fun bind(item: Exercise) {
+            binding.title.text = item.title
+            binding.difficulty.text = item.difficulty
+            binding.topicCount.text = Utils.getAdapterNumberLabel(adapterPosition + 1)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding = ItemExercisesBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding, clickListener)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind((getItem(position)))
+    }
+
+}
---
+++ app/src/main/java/com/olabode/wilson/pytutor/ui/exercise/ExerciseListFragment.kt
@@ -0,0 +1,14 @@
+package com.olabode.wilson.pytutor.ui.exercise
+
+import androidx.fragment.app.Fragment
+import com.olabode.wilson.pytutor.R
+import com.olabode.wilson.pytutor.databinding.FragmentExerciseListBinding
+import com.olabode.wilson.pytutor.extensions.viewBinding
+
+/**
+ * Created by Ogheneruona Onobrakpeya on 10/10/20.
+ */
+class ExerciseListFragment : Fragment(R.layout.fragment_exercise_list) {
+    private val binding by viewBinding(FragmentExerciseListBinding::bind)
+
+}
