35041f1d64dae9392276e9cd01f2cd3ce2e77e05
Adeyoriju Olabode Wilson <whilson03@gmail.com>
2020-10-21 22:55:03 +0100

    added support for syntax highlighting in lessons
    Signed-off-by: Adeyoriju Olabode Wilson <whilson03@gmail.com>

--- app/src/main/java/com/olabode/wilson/pytutor/ui/tutorial/adapters/LessonViewHolders.kt
+++ app/src/main/java/com/olabode/wilson/pytutor/ui/tutorial/adapters/LessonViewHolders.kt
@@ -1,21 +1,27 @@
 package com.olabode.wilson.pytutor.ui.tutorial.adapters
 
+import android.content.Context
 import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
 import com.olabode.wilson.pytutor.databinding.ItemBulletTextBinding
 import com.olabode.wilson.pytutor.databinding.ItemCodeSnippetBinding
 import com.olabode.wilson.pytutor.databinding.ItemTutorialBodyBinding
 import com.olabode.wilson.pytutor.models.tutorial.Tutorial
+import com.olabode.wilson.pytutor.views.PythonSyntaxManager
 
 /**
  *   Created by OLABODE WILSON on 9/9/20.
  */
 
-class CodeViewHolder(val binding: ItemCodeSnippetBinding) : RecyclerView.ViewHolder(binding.root) {
+class CodeViewHolder(val binding: ItemCodeSnippetBinding, context: Context) : RecyclerView.ViewHolder(binding.root) {
+
+    init {
+        PythonSyntaxManager.applyMonokaiTheme(context, binding.codeView)
+    }
 
     fun bind(item: Tutorial) {
         item.codeSnippet?.let { code ->
-            binding.codeText.text = code.code
+            binding.codeView.setText(code.code)
 
             if (code.header.isNotEmpty()) {
                 binding.header.text = code.header
