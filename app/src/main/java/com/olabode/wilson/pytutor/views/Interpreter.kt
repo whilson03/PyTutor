1d3fa818891e2efb6eca16e860671eac64c901d6
Adeyoriju Olabode Wilson <whilson03@gmail.com>
2020-10-26 05:44:01 +0100

    implemented support for termination of infinite loops and getting user input
    Signed-off-by: Adeyoriju Olabode Wilson <whilson03@gmail.com>

--- app/src/main/java/com/olabode/wilson/pytutor/views/Interpreter.kt
+++ app/src/main/java/com/olabode/wilson/pytutor/views/Interpreter.kt
@@ -28,65 +28,114 @@ class Interpreter @JvmOverloads constructor(
     }
 
     private fun setUpPage(): String {
-        return """ 
-            <html> 
-<head> 
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<script src="www/jquery-3.5.1.min.js" type="text/javascript"></script> 
-<script src="www/skulpt.min.js" type="text/javascript"></script> 
-<script src="www/skulpt-stdlib.js" type="text/javascript"></script> 
+        return """
+            
+           <html> 
 
-</head> 
-<body> 
+    <style type="text/css">
+         #outer
+        {
+            width:100%;
+            text-align: start;
+        }
+        .inner
+        {
+            display: inline-block;
+        }
+    </style>
 
-<script type="text/javascript"> 
-// output functions are configurable.  This one just appends some text
-// to a pre element.
-function outf(text) { 
-    var mypre = document.getElementById("output"); 
-    mypre.innerHTML = mypre.innerHTML + text; 
-} 
-function builtinRead(x) {
-    if (Sk.builtinFiles === undefined || Sk.builtinFiles["files"][x] === undefined)
-            throw "File not found: '" + x + "'";
-    return Sk.builtinFiles["files"][x];
-}
+        <head> 
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <script src="www/jquery-3.5.1.min.js" type="text/javascript"></script> 
+        <script src="www/skulpt.min.js" type="text/javascript"></script> 
+        <script src="www/skulpt-stdlib.js" type="text/javascript"></script> 
+        </head> 
 
-// Here's everything you need to run a python program in skulpt
-// grab the code from your textarea
-// get a reference to your pre element for output
-// configure the output function
-// call Sk.importMainWithBody
-function run(code) { 
-   var prog = code;
-   var mypre = document.getElementById("output"); 
-   mypre.innerHTML = ''; 
-   Sk.pre = "output";
-   Sk.configure({output:outf, read:builtinRead}); 
-   (Sk.TurtleGraphics || (Sk.TurtleGraphics = {})).target = 'mycanvas';
-   var myPromise = Sk.misceval.asyncToPromise(function() {
-       return Sk.importMainWithBody("<stdin>", false, prog, true);
-   });
-   myPromise.then(function(mod) {
-       console.log('success');
-   },
-       function(err) {
-       console.log(err.toString());
-       mypre.innerHTML = err.toString();
-   });
-} 
-</script> 
+    <body> 
+        
 
-</form> 
+        <script type="text/javascript"> 
 
-<pre id="output" ></pre> 
-<!-- If you want turtle graphics include a canvas -->
-<div id="mycanvas"></div> 
+        var StopExecution = false;
+        // output functions are configurable.  This one just appends some text
+        // to a pre element.
+        function outf(text) { 
+            var mypre = document.getElementById("output"); 
+            mypre.innerHTML = mypre.innerHTML + text; 
+        } 
 
-</body> 
+        function builtinRead(x) {
+            if (Sk.builtinFiles === undefined || Sk.builtinFiles["files"][x] === undefined)
+                    throw "File not found: '" + x + "'";
+            return Sk.builtinFiles["files"][x];
+        }
 
 
+
+        Sk.configure({
+          output: outf,
+          // yieldLimit: 100,
+          // execLimit: 5000,
+          killableWhile: true,
+          killableFor: true,
+          
+          inputTakesPrompt: true,
+          // you can customise the input function - this can return a promise - the default is just to use window.prompt
+          inputfun: function (args) {
+            return new Promise((resolve, reject) => {
+              setTimeout(function() {
+                resolve(window.prompt(args))
+              }, 250)
+            })
+          },
+          read: builtinRead,
+          __future__: Sk.python3
+        });
+
+
+        function run(code) {
+          StopExecution = false;
+          const prog = code;
+          const output = document.getElementById("output");
+          const status = document.getElementById("status");
+          output.innerHTML = "";
+          status.innerHTML = "Running" ;
+
+          Sk.misceval.asyncToPromise(() =>
+            Sk.importMainWithBody("<stdin>", false, prog, true), {
+              // handle a suspension of the executing code
+              // "*" says handle all types of suspensions
+              "*": () => {
+                if (StopExecution) throw "Execution interrupted"
+              }
+            },
+          ).catch(err => {
+            outf("\n" + err.toString());
+          }).finally(() => {
+            status.innerHTML = "Stopped" ;
+          })
+        }
+
+        
+
+        function stopit() {
+          StopExecution = true;
+        }
+
+        </script> 
+
+        <div id="outer">
+          <div class="inner"><label id="status"><small>Running</small></label></div>
+          <div class="inner"><button onclick="stopit()" type="button">Stop</button></div>
+         
+        </div>
+     <pre id="output" ></pre> 
+
+    </body> 
+        
+        
 </html> 
+                    
             
             
         """
