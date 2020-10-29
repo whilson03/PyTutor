97ec2b848495dd34bb1f13f269c1f28c59395b06
Adeyoriju Olabode Wilson <whilson03@gmail.com>
2020-10-29 16:58:22 +0100

    refactored some code to use extension function and code cleanup
    Signed-off-by: Adeyoriju Olabode Wilson <whilson03@gmail.com>

--- app/src/main/java/com/olabode/wilson/pytutor/ui/tutorial/LessonCompletionFragment.kt
+++ app/src/main/java/com/olabode/wilson/pytutor/ui/tutorial/LessonCompletionFragment.kt
@@ -1,6 +1,5 @@
 package com.olabode.wilson.pytutor.ui.tutorial
 
-import android.animation.ObjectAnimator
 import android.graphics.Color
 import android.os.Bundle
 import android.util.DisplayMetrics
@@ -13,9 +12,11 @@ import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.olabode.wilson.pytutor.R
 import com.olabode.wilson.pytutor.databinding.FragmentLessonCompletionBinding
+import com.olabode.wilson.pytutor.extensions.showAndAnimateRating
 import com.olabode.wilson.pytutor.extensions.viewBinding
 import com.olabode.wilson.pytutor.models.Topic
 import com.olabode.wilson.pytutor.ui.tutorial.viewmodel.CompletedLessonViewModel
+import com.olabode.wilson.pytutor.utils.Utils
 import com.olabode.wilson.pytutor.utils.states.DataState
 import dagger.hilt.android.AndroidEntryPoint
 import nl.dionsegijn.konfetti.models.Shape
@@ -32,27 +33,31 @@ class LessonCompletionFragment : Fragment(R.layout.fragment_lesson_completion) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val display = DisplayMetrics()
-        requireActivity().windowManager.defaultDisplay.getMetrics(display)
+
+        showConfetti()
+
         val score = args.score
         val numberOfQuestions = args.numberOfQuestions
         val topic = args.topic
 
-        val scoreRating = getRating(score.toFloat(), numberOfQuestions.toFloat())
-        setUpRating(scoreRating)
+        val scoreRating = Utils.calculateRating(score.toFloat(), numberOfQuestions.toFloat())
+        binding.ratingBar.showAndAnimateRating(scoreRating)
 
         viewModel.onCourseCompleted(topic.topicId, scoreRating, topic.orderKey)
-            .observe(viewLifecycleOwner, Observer {
-                when (it) {
-                    is DataState.Success -> {
-                        Timber.d("SUCCESS")
-                    }
+                .observe(viewLifecycleOwner, Observer {
+                    when (it) {
+                        is DataState.Success -> {
+                            Timber.d("SUCCESS")
+                        }
 
-                    is DataState.Error -> {
-                        /* no-op */
+                        is DataState.Error -> {
+                            Timber.d(it.message)
+                        }
+                        else -> {
+
+                        }
                     }
-                }
-            })
+                })
 
         viewModel.getNextTopic(topic.orderKey).observe(viewLifecycleOwner, Observer { result ->
             when (result) {
@@ -69,21 +74,12 @@ class LessonCompletionFragment : Fragment(R.layout.fragment_lesson_completion) {
 
         binding.nextLesson.setOnClickListener {
             findNavController().navigate(
-                LessonCompletionFragmentDirections
-                    .actionLessonCompletionFragmentToLessonGraph(nextTopic.title, nextTopic)
+                    LessonCompletionFragmentDirections
+                            .actionLessonCompletionFragmentToLessonGraph(nextTopic.title, nextTopic)
             )
         }
 
-        binding.viewKonfetti.build()
-            .addColors(Color.YELLOW, Color.GREEN, Color.MAGENTA)
-            .setDirection(0.0, 359.0)
-            .setSpeed(1f, 5f)
-            .setFadeOutEnabled(true)
-            .setTimeToLive(2000L)
-            .addShapes(Shape.Square, Shape.Circle)
-            .addSizes(Size(12))
-            .setPosition(-50f, display.widthPixels + 50f, -50f, -50f)
-            .streamFor(300, 5000L)
+
 
 
         binding.home.setOnClickListener {
@@ -91,20 +87,19 @@ class LessonCompletionFragment : Fragment(R.layout.fragment_lesson_completion) {
         }
     }
 
-    private fun setUpRating(value: Float) {
-        val current: Float = binding.ratingBar.rating
-        val anim = ObjectAnimator.ofFloat(binding.ratingBar, "rating", current, value)
-        anim.duration = 2000
-        anim.start()
+    private fun showConfetti() {
+        val display = DisplayMetrics()
+        requireActivity().windowManager.defaultDisplay.getMetrics(display)
+        binding.viewKonfetti.build()
+                .addColors(Color.YELLOW, Color.GREEN, Color.MAGENTA)
+                .setDirection(0.0, 359.0)
+                .setSpeed(1f, 5f)
+                .setFadeOutEnabled(true)
+                .setTimeToLive(2000L)
+                .addShapes(Shape.Square, Shape.Circle)
+                .addSizes(Size(12))
+                .setPosition(-50f, display.widthPixels + 50f, -50f, -50f)
+                .streamFor(300, 2000L)
     }
 
-    private fun getRating(score: Float, numberOfQuestions: Float): Float {
-        // Convert score to star rating
-        val maxRating = NO_STARS
-        return maxRating / numberOfQuestions * score
-    }
-
-    companion object {
-        const val NO_STARS = 3
-    }
 }
