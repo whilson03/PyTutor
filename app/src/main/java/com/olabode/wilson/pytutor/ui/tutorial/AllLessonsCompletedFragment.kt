2545449fc135874c463f21b2ad517ff90b7b44af
rhodeon <ruonabraks@gmail.com>
2020-11-02 00:06:55 +0100

    Added AllLessonsCompleted fragment and layout.

---
+++ app/src/main/java/com/olabode/wilson/pytutor/ui/tutorial/AllLessonsCompletedFragment.kt
@@ -0,0 +1,47 @@
+package com.olabode.wilson.pytutor.ui.tutorial
+
+import android.graphics.Color
+import android.os.Bundle
+import android.util.DisplayMetrics
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.olabode.wilson.pytutor.R
+import com.olabode.wilson.pytutor.databinding.FragmentAllLessonsCompletedBinding
+import com.olabode.wilson.pytutor.extensions.viewBinding
+import dagger.hilt.android.AndroidEntryPoint
+import nl.dionsegijn.konfetti.models.Shape
+import nl.dionsegijn.konfetti.models.Size
+
+/**
+ * Created by Ogheneruona Onobrakpeya on 11/1/20.
+ */
+
+class AllLessonsCompletedFragment : Fragment(R.layout.fragment_all_lessons_completed) {
+    private val binding by viewBinding(FragmentAllLessonsCompletedBinding::bind)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        showConfetti()
+
+        binding.home.setOnClickListener {
+            findNavController().popBackStack()
+        }
+    }
+
+    private fun showConfetti() {
+        val display = DisplayMetrics()
+        requireActivity().windowManager.defaultDisplay.getMetrics(display)
+        binding.viewKonfetti.build()
+                .addColors(Color.YELLOW, Color.GREEN, Color.MAGENTA)
+                .setDirection(0.0, 359.0)
+                .setSpeed(1f, 5f)
+                .setFadeOutEnabled(true)
+                .setTimeToLive(2000L)
+                .addShapes(Shape.Square, Shape.Circle)
+                .addSizes(Size(12))
+                .setPosition(-50f, display.widthPixels + 50f, -50f, -50f)
+                .streamFor(300, 2000L)
+    }
+}
